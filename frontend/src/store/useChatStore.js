bac35ae1c817f2e8287082ababd94f3ca11f8783
hqrris0n <153888672+hqrris0n@users.noreply.github.com>
2025-05-26 20:44:00 -0500

    Implemented chat feature

--- frontend/src/store/useChatStore.js
+++ frontend/src/store/useChatStore.js
@@ -2,7 +2,7 @@ import {create} from 'zustand';
 import {toast} from 'react-hot-toast';
 import {axiosInstance} from '../lib/axios';
 
-export const useChatStore = create((set) => ({
+export const useChatStore = create((set, get) => ({
   messages:[],
   users: [],
   selectedUser: null,
@@ -29,9 +29,17 @@ export const useChatStore = create((set) => ({
     } catch (error) {
       toast.error(error.response?.data?.message || "Failed to fetch messages");
     } finally {
-      set({isUsersLoading: false});
+      set({isMessagesLoading: false});
+    }
+  },
+  sendMessage: async(messageData) => {
+    const {selectedUser, messages} = get();
+    try {
+      const res = await axiosInstance.post(`/messages/send/${selectedUser._id}`, messageData);
+      set({messages: [...messages, res.data]});
+    } catch (error) {
+      toast.error(error.response?.data?.message || "Failed to send message");
     }
   },
   setSelectedUser: (user) => set({selectedUser: user}),
-
 }));
